4ef1bdff1640de50778aad6e54c0eba771bff2ac
Sarahstylez <sarahgee@my.capilanou.ca>
2024-06-08 00:29:31 -0700

    testing font families

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,12 @@
 import "./LandingPage.scss";
 
 function LandingPage() {
-  return <div className="landing"></div>;
+  return (
+    <div className="landing">
+      <h1>hello!</h1> <h2>hello!</h2> <h3>hello!</h3> <h4>hello!</h4>
+      <h5>hello!</h5> <p>hello!</p>
+    </div>
+  );
 }
 
 export default LandingPage;
