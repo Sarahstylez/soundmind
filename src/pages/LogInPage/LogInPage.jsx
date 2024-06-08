c9fff40669c6a2867253a923ef043fb6dd8dcb5f
Sarahstylez <sarahgee@my.capilanou.ca>
2024-06-08 13:31:15 -0700

    add bg gradient and loginpage styles

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,11 +1,25 @@
 import "./LogInPage.scss";
+import Logo from "../../assets/logos/logo-small.svg";
+import ProfileImage from "../../assets/images/profile-pic-large.svg";
 
 function LogInPage() {
   return (
-    <div>
-      <h1>Log In Page</h1>
-      {/* Add your log in form here */}
-    </div>
+    <>
+      <section className="login">
+        <div className="title">
+          <img src={Logo} alt="small soundmind logo" />
+          <div className="title__text">
+            <h2>learn about yourself,</h2>
+            <h2>and adhd.</h2>
+          </div>
+        </div>
+        <img
+          src={ProfileImage}
+          alt="Alex Sharp's profile image"
+          className="profile-image"
+        />
+      </section>
+    </>
   );
 }
 
